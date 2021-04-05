4a4eb4902ce61d002996fe280f42aa91a33c827b
jayan <bathla.jayan@gmail.com>
2021-04-06 04:14:39 +0530

    merging aniket changes

--- main.js
+++ main.js
@@ -15,3 +15,33 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
+
+var splashCheck = true;
+var splash = document.querySelector('div.splash');
+var loading = document.querySelector('div.loading');
+function removeSplashDisplay(){
+  splash.classList.add('display-none');
+
+  // Show Pop Up Modal
+  $(document).ready(function() {
+    $('#global-modal').modal('show');
+  });
+}
+function removeSplashOpacity(){
+  splash.classList.add('opacity-zero');
+  splashCheck = false;
+  if(!splashCheck){
+    loading.classList.add('display-none');
+  }
+  setTimeout(removeSplashDisplay,200);
+}
+document.addEventListener('DOMContentLoaded',function(){
+  splashCheck = true;
+  setTimeout(removeSplashOpacity,2000);
+});
+document.onload = setTimeout(function(){
+  if(splashCheck){
+    loading.classList.remove('display-none');
+    splash = false;
+  }
+},4000);
